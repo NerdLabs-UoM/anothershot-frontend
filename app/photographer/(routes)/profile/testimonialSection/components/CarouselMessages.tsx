7cbc3fe1f3ca3a1257f24b10e606b6bd577cc163
PawaniGimhani <125270084+PawaniGimhani@users.noreply.github.com>
2024-01-25 23:09:54 +0530

    fix(ANSH-92): update styling for testimonial section

--- app/photographer/(routes)/profile/testimonialSection/components/CarouselMessages.tsx
+++ app/photographer/(routes)/profile/testimonialSection/components/CarouselMessages.tsx
@@ -1,69 +1,89 @@
-"use client"
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious,type CarouselApi} from "@/components/ui/carousel"
-import { Card, CardContent } from "@/components/ui/card"
+"use client";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+  type CarouselApi,
+} from "@/components/ui/carousel";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar"
+import { Card, CardContent } from "@/components/ui/card";
 import Image from "next/image";
-import React, { Component,useState, useEffect } from 'react'
+import React, { useState } from "react";
 
+interface TestimonialsData {
+  id: string;
+  name: string;
+  feedback: string;
+  url: string;
+  visibility: boolean;
+}
 
-const testimonialsData = [
-  {
-    id: 1,
-    name: 'Esther Howard',
-    message: 'The quality of their work is outstanding. The attention to detail and their editing skills truly  I am always impressed and the whole process, from booking to receiving the final photos, was seamless.',
-    url: '/images/avatar.png'
-  },
-  {
-    id: 2,
-    name: 'John Smith',
-    message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.',
-    url:'/images/ellipse.png'
-  },
+interface CarouselMessagesProps {
+  testimonialsData: TestimonialsData[];
+}
 
-];
+const CarouselMessages: React.FC<CarouselMessagesProps> = ({
+  testimonialsData,
+}) => {
+  const [testimonials, setTestimonials] = useState(testimonialsData);
+  const [api, setApi] = React.useState<CarouselApi>();
+  const [current, setCurrent] = React.useState(0);
+  const [count, setCount] = React.useState(0);
 
-const CarouselMessages =() => {
-  
-  
-    const [api, setApi] = React.useState<CarouselApi>()
-    const [current, setCurrent] = React.useState(0)
-    const [count, setCount] = React.useState(0)
-
-    React.useEffect(() => {
-      if (!api) {
-        return
-      }
-   
-      setCount(api.scrollSnapList().length)
-      setCurrent(api.selectedScrollSnap() + 1)
-   
-      api.on("select", () => {
-        setCurrent(api.selectedScrollSnap() + 1)
-      })
-    }, [api])
-
-    return (
-      <div className="flex flex-col">
-        <Carousel className="flex flex-col items-start gap-12 flex-1 self-stretch" setApi={setApi}>
-          <CarouselContent> 
-            {testimonialsData.map((testimonial) => (
+  React.useEffect(() => {
+    if (!api) {
+      return;
+    }
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
+  const visibleTestimonials = testimonialsData.filter(
+    (testimonial) => testimonial.visibility
+  );
+  return (
+    <div className="flex flex-col">
+      {visibleTestimonials.length > 0 && (
+        <Carousel
+          className="flex flex-row items-start gap-12 flex-1 self-stretch mb-10 w-[360px] sm:w-[550px] md:w-[700px] lg:w-[920px]"
+          setApi={setApi}
+        >
+          <CarouselContent className="w-[365px] sm:w-[565px] md:w-[715px] lg:w-[935px]">
+            {visibleTestimonials.map((testimonial) => (
               <CarouselItem key={testimonial.id}>
                 <div className="p-1">
-                  <Card >
-                    <CardContent className="flex flex-col p-[40px] ">
+                  <Card>
+                    <CardContent className="flex flex-col p-[12px] md:p-[20px] lg:p-[40px] ">
                       <div>
                         <Image
                           src="/images/com.png"
                           alt="Description of the image"
                           width={39}
                           height={29}
+                          className="w-[20px] h-[15px] sm:w-[30px] sm:h-[25px] md:w-[39px] md:h-[29px]"
                         />
                       </div>
-                      <div className="flex p-0 items-center gap-10 flex-1">
-                        <span className="flex flex-col justify-center items-end gap-3 flex-1 self-stretch text-right text-slate-950">{testimonial.message}</span>
-                        <img src={testimonial.url} width={100} height={100}/>
+                      <div className="flex flex-col items-start md:flex-row p-0 md:items-center gap-10 flex-1">
+                        <span className="flex flex-col justify-center items-end gap-3 flex-1 self-stretch text-right text-slate-950 text-sm sm:text-base">
+                          {testimonial.feedback}
+                        </span>
+                        <Avatar className="w-[70px] h-[70px] sm:w-[100px] sm:h-[100px]" >
+                          <AvatarImage src={testimonial.url} alt="@shadcn" />
+                          <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
                       </div>
                       <div>
-                        <span className="text-slate-950 text-right font-bold text-2xl">{testimonial.name}</span>
+                        <span className="text-slate-950 text-right font-bold text-base sm:text-2xl">
+                          {testimonial.name}
+                        </span>
                       </div>
                     </CardContent>
                   </Card>
@@ -74,10 +94,8 @@ const CarouselMessages =() => {
           <CarouselPrevious />
           <CarouselNext />
         </Carousel>
-
-      </div>
-    )
-  
-}
-
-export default CarouselMessages
+      )}
+    </div>
+  );
+};
+export default CarouselMessages;
