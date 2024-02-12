f3c41c61d7df38474a57a02dc13d348fc56ddb32
PawaniGimhani <125270084+PawaniGimhani@users.noreply.github.com>
2024-02-13 02:30:27 +0530

    feat(feed): implement feed component

---
+++ app/user/(routes)/photographer/[userId]/(routes)/feed/components/FeedComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FeedComponent = () =>{
+    return (
+        <div>
+            <h1>Feed Component</h1>
+        </div>
+    )
+}
+
+export default FeedComponent;
