02b592258c700878d1346045f4da4a33ce38bc6b
Gimhanee S.P <125270084+pawanigimhani@users.noreply.github.com>
2024-02-28 02:50:39 +0530

    Bugfix/ANSH-103/testimonial-fix-styles-p3 (#20)
    * fix(testimonial): fix styles
    
    * style: update styles for responsive layouts
    
    * feat: add axios configuration for API integration
    
    * fix: handle 404 error when submitting testimonial
    
    * feat: update the api for testimonial visibility
    
    * feat(edit-button): add updates to the edit button component
    
    * feat(api): update testimonial endpoints
    
    ---------
    
    Co-authored-by: Kalhara JA <kalharajak.21@uom.lk>

--- app/user/(routes)/photographer/[userId]/(routes)/profile/testimonialSection/testimonialSection.tsx
+++ app/user/(routes)/photographer/[userId]/(routes)/profile/testimonialSection/testimonialSection.tsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect, use } from "react";
+import { useParams } from "next/navigation";
+import axios from "axios";
 
 import CarouselMessages from "./components/CarouselMessages";
 import EditButton from "./components/EditButton";
@@ -6,60 +8,43 @@ import SubmitForm from "./components/SubmitForm";
 
 interface TestimonialsData {
   id: string;
-  name: string;
-  feedback: string;
-  url: string;
-  visibility: boolean;
+  review: string;
+  rating: number;
+  visibility: 'PUBLIC' | 'PRIVATE';
+  client: {
+    id: string;
+    name: string;
+    user: {
+      image: string | null; 
+    };
+  };
 }
-const testimonialsData: TestimonialsData[] = [
-  {
-    id: "1",
-    name: "Esther Howard",
-    feedback:
-      "The quality of their work is outstanding. The attention to detail and their editing skills truly  I am always impressed and the whole process, from booking to receiving the final photos, was seamless.",
-    url: "/images/avatar.png",
-    visibility: true,
-  },
-  {
-    id: "2",
-    name: "John Smith",
-    feedback:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.",
-    url: "/images/ellipse.png",
-    visibility: true,
-  },
-  {
-    id: "3",
-    name: "Nick Smith",
-    feedback:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.",
-    url: "/images/ellipse.png",
-    visibility: true,
-  },
-  {
-    id: "4",
-    name: "Ann Smith",
-    feedback:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.",
-    url: "/images/ellipse.png",
-    visibility: true,
-  },
-];
+
 const TestMonialsSection = () => {
-  const [testimonials, setTestimonials] = useState(testimonialsData);
+  const [testimonials, setTestimonials] = useState<TestimonialsData[]>([]);
+  const { userId } = useParams();
+  useEffect(() => {
+    const fetchTestimonials = async () => {
+      try {
+        const response = await axios.get<TestimonialsData[]>(`${process.env.NEXT_PUBLIC_API_URL}/api/photographer/${userId}/profile/testimonials`);
+        setTestimonials(response.data);
+      } catch (error:any) {
+        console.error('Error fetching testimonials:', error);
+      }
+    };
+
+    fetchTestimonials();
+  }, [userId]);
+
   return (
-    <div className="flex flex-col items-center max-w-5xl border-y border-transparent py-40">
+    <div className="flex flex-col items-center w-full border-y border-transparent pb-40 pt-32">
       <EditButton
         testimonialsData={testimonials}
-        setTestimonialsData={setTestimonials}
       />
       <CarouselMessages 
         testimonialsData={testimonials} 
       />
-      <SubmitForm
-        testimonialsData={testimonials}
-        setTestimonialsData={setTestimonials}
-      />
+      <SubmitForm/>
     </div>
   );
 };
