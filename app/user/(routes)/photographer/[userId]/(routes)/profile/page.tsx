2ad72d959cb7f3963a8d3446dfee17b098dd21d0
Amarasena B.H.L.M. <lahimalshi@gmail.com>
2024-02-27 14:39:05 +0530

    feat(ANSH-96): add edit modal  functionality for featuredPhotos  section

--- app/user/(routes)/photographer/[userId]/(routes)/profile/page.tsx
+++ app/user/(routes)/photographer/[userId]/(routes)/profile/page.tsx
@@ -20,7 +20,6 @@ const PhotographerProfile = () => {
         <>
           <Hero />
           <FeaturedPhotoSection />
-        
           <ContactSection />
           <TestMonialsSection />
         
