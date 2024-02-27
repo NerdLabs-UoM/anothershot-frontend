7274b1105d14a264f8d6412794b86d92ca94803b
Gimhanee S.P <125270084+pawanigimhani@users.noreply.github.com>
2024-02-28 02:50:39 +0530

    Bugfix/ANSH-103/testimonial-fix-styles-p3 (#20)
    * fix(testimonial): fix styles
    
    * style: update styles for responsive layouts
    
    * feat: add axios configuration for API integration
    
    * fix: handle 404 error when submitting testimonial
    
    * feat: update the api for testimonial visibility
    
    * feat(edit-button): add updates to the edit button component
    
    * feat(api): update testimonial endpoints
    
    ---------
    
    Co-authored-by: Kalhara JA <kalharajak.21@uom.lk>

--- app/user/(routes)/photographer/[userId]/(routes)/profile/testimonialSection/components/EditButton.tsx
+++ app/user/(routes)/photographer/[userId]/(routes)/profile/testimonialSection/components/EditButton.tsx
@@ -1,6 +1,9 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useParams } from "next/navigation";
+import axios from "axios";
 import { toast } from "react-hot-toast";
 import { Check, ChevronsUpDown, Pencil } from "lucide-react";
 import {
@@ -36,38 +39,68 @@ import {
   AvatarFallback,
   AvatarImage,
 } from "@/components/ui/avatar"
+import { isEqual } from 'lodash';
 import { cn } from "@/app/lib/utils";
 
 interface TestimonialsData {
   id: string;
-  name: string;
-  feedback: string;
-  url: string;
-  visibility: boolean;
+  review: string;
+  rating: number;
+  visibility: 'PUBLIC' | 'PRIVATE';
+  client: {
+    id: string;
+    name: string;
+    user: {
+      image: string | null;
+    };
+  };
 }
 
 interface EditButtonProps {
   testimonialsData: TestimonialsData[];
-  setTestimonialsData: React.Dispatch<React.SetStateAction<TestimonialsData[]>>;
+  
 }
 
 const EditButton: React.FC<EditButtonProps> = ({
   testimonialsData,
-  setTestimonialsData,
 }) => {
+  const { userId } = useParams();
+  const { data: session } = useSession();
   const [testimonials, setTestimonials] = useState(testimonialsData);
   const [checked, setChecked] = useState(false);
   const [open, setOpen] = React.useState(false);
   const [value, setValue] = React.useState("");
+  const [loading, setLoading] = useState(false);
 
-  const handleSubmit = () => {
-    setTestimonialsData(testimonials);
-    toast.success("Testimonials updated successfully!");
+  useEffect(() => {
+    setTestimonials(testimonialsData);
+  }, [testimonialsData]);
+
+  const handleSubmit = async () => {
+    setLoading(true);
+    try {
+      const changedTestimonials = testimonials.filter(testimonial =>
+        testimonial.visibility !== testimonialsData.visibility
+      );
+      const changedTestimonialsIds = changedTestimonials.map(testimonial => testimonial.id);
+      const response = await axios.patch(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/photographer/${userId}/profile/testimonials/visibility`,
+        { testimonialId: changedTestimonialsIds }
+      );
+      console.log("Testimonials updated successfully:", response.data);
+      toast.success("Testimonials updated successfully!");
+    } catch (error) {
+      console.error("Error updating testimonials:", error);
+      toast.error("Failed to update testimonials");
+    } finally {
+      setLoading(false);
+    }
   };
+
   const handleVisibility = () => {
     const items = [...testimonials];
     const testimonial = items.find((testimonial) => value === testimonial.id);
-    testimonial!.visibility = !testimonial?.visibility;
+    testimonial!.visibility = isEqual(testimonial?.visibility, "PUBLIC") ? "PRIVATE" : "PUBLIC";
     setTestimonials(items);
     setChecked(!checked);
   };
@@ -75,12 +108,43 @@ const EditButton: React.FC<EditButtonProps> = ({
     const testimonial = testimonials.find(
       (testimonial) => value === testimonial.id
     );
-    testimonial?.visibility ? setChecked(true) : setChecked(false);
+    isEqual(testimonial?.visibility, "PUBLIC") ? setChecked(true) : setChecked(false);
   };
-  return (
-    <div className="grid grid-cols-1 sm:grid-cols-5 xl:flex xl:flex-row xl:justify-between xl:space-x-[1024px] mb-16">
-      <p className="font-extrabold text-2xl md:text-5xl">Testimonials</p>
-      <Dialog>
+  const renderStars = (rating: number) => {
+    const stars = [];
+    for (let i = 0; i < 5; i++) {
+      if (i < rating) {
+        stars.push(
+          <svg
+            key={i}
+            className="w-3 h-3 text-yellow-300"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 22 20"
+          >
+            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+          </svg>
+        );
+      } else {
+        stars.push(
+          <svg
+            key={i}
+            className="w-3 h-3 text-gray-300"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 22 20"
+          >
+            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+          </svg>
+        );
+      }
+    }
+    return stars;
+  };
+
+  const renderEditButton = () => {
+    if (session?.user?.id === userId) {
+      return (
         <DialogTrigger className="sm:col-span-4 sm:flex sm:justify-end ">
           <Button
             variant={"outline"}
@@ -90,6 +154,16 @@ const EditButton: React.FC<EditButtonProps> = ({
             <Pencil />
           </Button>
         </DialogTrigger>
+      );
+    }
+    return null;
+  };
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-5 mb-16 mx-3">
+      <p className="font-extrabold text-2xl md:text-5xl">Testimonials</p>
+      <Dialog>
+        {renderEditButton()}
         <DialogContent className="sm:max-w-[425px]">
           <DialogHeader>
             <DialogTitle>Edit Testmonials</DialogTitle>
@@ -97,7 +171,7 @@ const EditButton: React.FC<EditButtonProps> = ({
               Make changes to your tesmonials here. Click save when you're done.
             </DialogDescription>
           </DialogHeader>
-          <div className="grid grid-cols-2 gap-6 py-4 ">
+          <div className="grid grid-cols-2 gap-20 sm:gap-6 py-4 ">
             <div className="grid grid-cols-4 items-center gap-4">
               <Popover open={open} onOpenChange={setOpen}>
                 <PopoverTrigger asChild>
@@ -110,7 +184,7 @@ const EditButton: React.FC<EditButtonProps> = ({
                     {value
                       ? testimonials.find(
                         (testimonial) => testimonial.id === value
-                      )?.name
+                      )?.client.name
                       : "Select testimonial..."}
                     <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
                   </Button>
@@ -137,12 +211,10 @@ const EditButton: React.FC<EditButtonProps> = ({
                               <Check
                                 className={cn(
                                   "mr-2 h-4 w-4",
-                                  testimonial.visibility
-                                    ? "opacity-100"
-                                    : "opacity-0"
+                                  isEqual(testimonial.visibility, "PUBLIC") ? "opacity-100" : "opacity-0"
                                 )}
                               />
-                              {testimonial.name}
+                              {testimonial.client.name}
                             </CommandItem>
                           </HoverCardTrigger>
                           <HoverCardContent className="w-[500px]">
@@ -156,17 +228,20 @@ const EditButton: React.FC<EditButtonProps> = ({
                             </div>
                             <div className="flex p-0 items-center gap-10 flex-1">
                               <span className="flex flex-col justify-center items-end gap-3 flex-1 self-stretch text-right text-slate-950 text-xs">
-                                {testimonial.feedback}
+                                {testimonial.review}
                               </span>
                               <Avatar className="w-[70px] h-[70px]" >
-                                <AvatarImage src={testimonial.url} alt="@shadcn" />
+                                <AvatarImage src={testimonial.client.user.image || undefined} alt="@shadcn" />
                                 <AvatarFallback>CN</AvatarFallback>
                               </Avatar>
                             </div>
                             <div>
                               <span className="text-slate-950 text-right font-bold text-xl">
-                                {testimonial.name}
+                                {testimonial.client.name}
                               </span>
+                            </div>
+                            <div className="flex items-center gap-2 mt-2">
+                              {renderStars(testimonial.rating)}
                             </div>
                           </HoverCardContent>
                         </HoverCard>
--- app/user/(routes)/photographer/[userId]/(routes)/profile/testimonialSection/components/SubmitForm.tsx
+++ app/user/(routes)/photographer/[userId]/(routes)/profile/testimonialSection/components/SubmitForm.tsx
@@ -2,6 +2,7 @@
 
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
 import { useForm } from "react-hook-form";
 import {
   Form,
@@ -13,111 +14,124 @@ import {
 } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 import React, { Component } from "react";
-import { v4 as uuidv4 } from "uuid";
+import { useSession } from "next-auth/react";
+import { useParams } from "next/navigation";
+import { toast } from "react-hot-toast";
 
-interface TestimonialsData {
-  id: string;
-  name: string;
-  feedback: string;
-  url: string;
-  visibility: boolean;
-}
-interface SubmitFormProps {
-  testimonialsData: TestimonialsData[];
-  setTestimonialsData: React.Dispatch<React.SetStateAction<TestimonialsData[]>>;
-}
+
 const formSchema = z.object({
-  feedback: z.string().max(300, "Give your feedback"),
-  rate: z.enum(["Amazing", "Excellent", "Good", "Ordinary", "Bad"]),
+  review: z.string().min(100, "Give your feedback should be between 100-200 characters").max(200, "Give your feedback should be between 100-200 characters"),
+  rating: z.number(),
 });
-const SubmitForm: React.FC<SubmitFormProps> = ({
-  setTestimonialsData,
-  testimonialsData,
+const SubmitForm: React.FC = ({
 }) => {
+  const [selectedRating, setSelectedRating] = React.useState<number | 0 >(0);
+  const { userId } = useParams();
+  const { data: session } = useSession();
+  const [loading, setLoading] = React.useState(false);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      feedback: "",
-      rate: "Excellent",
+      review: "",
+      rating: 0,
     },
   });
-  const onSubmit = (values: z.infer<typeof formSchema>) => {
-    console.log(values);
-    const newTestimonial = {
-      id: uuidv4(),
-      name: "New User",
-      feedback: values.feedback,
-      url: "/images/ellipse.png", 
-      visibility: true,
-    };
-    setTestimonialsData((prevTestimonials) => [
-      ...prevTestimonials,
-      newTestimonial,
-    ]);
+  const handleStarClick = (ratingValue: number) => {
+    setSelectedRating(ratingValue);
+  };
+  const onSubmit = async (values: z.infer<typeof formSchema>, e:any) => {
+    try {
+      setLoading(true);
+      if (values) {
+        const response = await axios.post(`http://localhost:8000/api/photographer/${userId}/profile/testimonial`, {
+          review: values.review,
+          rating: selectedRating,
+          clientId: session?.user?.id,
+          photographerId:userId,
+        });
+        if (response.status === 200) {
+          setLoading(false);
+          e.target.reset();
+          toast.success("Thank you for your feedback");
+        }
+      }
+    } catch (error) {
+      setLoading(false);
+      toast.error("Something went wrong");
+    }finally{
+      setLoading(false);
+    }
+    form.reset();
+    setSelectedRating(0);
+  };
+
+  const renderForm = () => {
+    if (session?.user?.id === userId) {
+      return null;
+    }
+    return (<Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="rating"
+          render={({ field }) => (
+            <FormItem className="flex flex-col items-start ml-0 sm:ml-20 sm:flex-row sm:items-center md:ml-0">
+              <FormLabel className="font-medium text-sm mr-[30px] inline-flex">
+                Rate your Experience
+              </FormLabel>
+              <FormControl>
+                <div className="flex items-cente">
+                  {[...Array(5)].map((_, index) => (
+                    <svg
+                      key={index}
+                      className={`w-4 h-4 ${selectedRating && index < selectedRating
+                        ? "text-yellow-300"
+                        : "text-gray-300"
+                        } ms-1 cursor-pointer`}
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 22 20"
+                      onClick={() => handleStarClick(index + 1)}
+                    >
+                      <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                    </svg>
+                  ))}
+                </div>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="review"
+          render={({ field }) => (
+            <FormItem className="flex flex-col ml-0 sm:ml-20 md:ml-0">
+              <FormLabel className="font-medium text-sm">Feedback</FormLabel>
+              <FormControl>
+                <Textarea
+                  className="h-[80px] w-[346px] sm:w-[537px] md:w-[689px] resize-none"
+                  placeholder="Type your feedback here"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button
+          type="submit"
+          className="ml-0 sm:ml-20 h-9 w-[346px] sm:h-11 sm:w-[537px] md:w-[530px]"
+        >
+          Submit
+        </Button>
+      </form>
+    </Form>)
   };
   return (
-    <div className="w-[689px]">
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="rate"
-            render={({ field }) => (
-              <FormItem className="flex flex-col items-start ml-44 sm:ml-20 sm:flex-row sm:items-center md:ml-0">
-                <FormLabel className="font-medium text-sm mr-[30px] inline-flex">
-                  Rate your Experience
-                </FormLabel>
-                <Select onValueChange={field.onChange}>
-                  <FormControl>
-                    <SelectTrigger className="w-[95px] h-[28px] px-1">
-                      <SelectValue placeholder="Excellent" />
-                    </SelectTrigger>
-                  </FormControl>
-                  <SelectContent>
-                    <SelectItem value="Amazing">Amazing</SelectItem>
-                    <SelectItem value="Excellent">Excellent</SelectItem>
-                    <SelectItem value="Good">Good</SelectItem>
-                    <SelectItem value="Ordinary">Ordinary</SelectItem>
-                    <SelectItem value="Bad">Bad</SelectItem>
-                  </SelectContent>
-                </Select>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="feedback"
-            render={({ field }) => (
-              <FormItem className="flex flex-col ml-44 sm:ml-20 md:ml-0">
-                <FormLabel className="font-medium text-sm">Feedback</FormLabel>
-                <FormControl>
-                  <Textarea
-                    className="h-[80px] w-[346px] sm:w-[537px] md:w-[689px] resize-none"
-                    placeholder="Type your feedback here"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button
-            type="submit"
-            className="ml-44 sm:ml-20 h-9 w-[346px] sm:h-11 sm:w-[537px] md:w-[530px]"
-          >
-            Submit
-          </Button>
-        </form>
-      </Form>
+    <div className="w-[340px] sm:w-[689px]">
+      {renderForm()}
     </div>
   );
 };
