7ce204e3ff75509543fb840c7b88998e62b1d0ff
Amarasena B.H.L.M <141402575+LahiruniMalshika@users.noreply.github.com>
2024-02-27 23:24:26 +0530

    Feature/ANSH-96/featured-photos-edit-modal (#22)
    * feat(ANSH-96): update Featured Photo Modal
    
    * feat(ANSH-96): update Featured Photo Modal
    
    * feat(ANSH-95): add edit modal  functionality for featuredPhotos  section
    
    * feat(ANSH-96): add edit modal  functionality for featuredPhotos  section
    
    * feat(ANSH-96): add edit modal  functionality for featuredPhotos  section
    
    * feat(ANSH-96): add edit modal  functionality for featuredPhotos  section
    
    ---------
    
    Co-authored-by: Kalhara JA <kalharajak.21@uom.lk>

--- components/ui/command.tsx
+++ components/ui/command.tsx
@@ -8,6 +8,7 @@ import { Search } from "lucide-react"
 import { cn } from "@/app/lib/utils"
 import { Dialog, DialogContent } from "@/components/ui/dialog"
 
+
 const Command = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive>,
   React.ComponentPropsWithoutRef<typeof CommandPrimitive>
