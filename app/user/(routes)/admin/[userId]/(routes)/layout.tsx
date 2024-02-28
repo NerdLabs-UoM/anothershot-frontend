2079979aa799c4e20d715e73caba33248c0e0b24
Kavindu Wijegunasekara <110486479+Kavinduprabasara@users.noreply.github.com>
2024-02-29 02:42:45 +0530

    Bugfix/ANSH-112/hero section bugfix (#27)
    * feat(ANSH-65): admin-file-structure-changed
    
    * fix(ANSH-65): herosection-type-errors-fixed
    
    * fix(ANSH-65): hero-section-code-fixes
    
    * fix(ANSH-112): hero-section-bug-fixes

---
+++ app/user/(routes)/admin/[userId]/(routes)/layout.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import Navbar from "@/components/Navbar";
+import { Button } from "@/components/ui/button";
+import { signOut } from "next-auth/react";
+import AdminHeader from "@/components/adminHeader";
+
+type UserLayoutProps = {
+    children: React.ReactNode;
+}
+
+const UserLayout = (props: UserLayoutProps) => {
+    return (
+        <>
+            <AdminHeader/>
+            {props.children}
+        </>
+    );
+}
+
+export default UserLayout;
