078fddd43f1e1bd6785b6adaf23b660c4862179b
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2024-03-03 00:11:06 +0530

    Feature/ansh 99/setting section photographer (#31)
    * fix(ANSH-99): fix setting section for photographer
    
    * fix(ANSH-99): fix adding category for photographer setting section

---
+++ app/user/(routes)/photographer/[userId]/(routes)/profile/settings/components/data-table.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import {
+    ColumnDef,
+    flexRender,
+    getCoreRowModel,
+    useReactTable,
+} from "@tanstack/react-table";
+
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
+
+import { columns } from "./paymentColumns";
+
+interface DataTableProps<TData, TValue> {
+    columns: ColumnDef<TData, TValue>[];
+    data: TData[];
+}
+
+export function DataTable<TData, TValue>({
+    columns,
+    data,
+}: DataTableProps<TData, TValue>) {
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+    });
+
+    return (
+        <div className="flex flex-wrap mx-auto border rounded-md sm:w-[300px] md:w-[700px] lg:w-[920px]">
+            <div className="py-5 mx-4 text-2xl font-semibold leading-none leading-7 tracking-tight">
+                Payments Summary
+            </div>
+            <Table>
+                <TableHeader className="text-xs text-gray-500 bg-gray-100">
+                    {table.getHeaderGroups().map((headerGroup) => (
+                        <TableRow key={headerGroup.id}>
+                            {headerGroup.headers.map((header) => {
+                                return (
+                                    <TableHead key={header.id}>
+                                        {header.isPlaceholder
+                                            ? null
+                                            : flexRender(
+                                                  header.column.columnDef
+                                                      .header,
+                                                  header.getContext()
+                                              )}
+                                    </TableHead>
+                                );
+                            })}
+                        </TableRow>
+                    ))}
+                </TableHeader>
+                <TableBody>
+                    {table.getRowModel().rows?.length ? (
+                        table.getRowModel().rows.map((row) => (
+                            <TableRow
+                                className="text-gray-400 text-sm font-normal font-['Inter'] leading-tight"
+                                key={row.id}
+                                data-state={row.getIsSelected() && "selected"}
+                            >
+                                {row.getVisibleCells().map((cell) => (
+                                    <TableCell key={cell.id}>
+                                        {flexRender(
+                                            cell.column.columnDef.cell,
+                                            cell.getContext()
+                                        )}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        ))
+                    ) : (
+                        <TableRow>
+                            <TableCell
+                                colSpan={columns.length}
+                                className="h-24 text-center"
+                            >
+                                No results.
+                            </TableCell>
+                        </TableRow>
+                    )}
+                </TableBody>
+            </Table>
+        </div>
+    );
+}
