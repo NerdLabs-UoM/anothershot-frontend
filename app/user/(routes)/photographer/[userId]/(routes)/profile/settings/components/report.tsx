0d73d06a567fe57ad183aa979ef0959eab2a5d6c
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2024-03-03 00:11:06 +0530

    Feature/ansh 99/setting section photographer (#31)
    * fix(ANSH-99): fix setting section for photographer
    
    * fix(ANSH-99): fix adding category for photographer setting section

---
+++ app/user/(routes)/photographer/[userId]/(routes)/profile/settings/components/report.tsx
@@ -0,0 +1,95 @@
+"use client"
+
+import {zodResolver} from "@hookform/resolvers/zod"
+import {useForm} from "react-hook-form"
+import * as z from "zod"
+
+import {Button} from "@/components/ui/button"
+import {Card, CardHeader, CardTitle, CardContent} from "@/components/ui/card";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import {Input} from "@/components/ui/input"
+import {Textarea} from "@/components/ui/textarea";
+import React from "react";
+
+
+const ReportFormSchema = z.object({
+    subject: z.string().max(100).min(1,{message: "Enter a subject"}),
+    description: z.string().max(300, ).min(1,{message: "Enter a Description"}),
+});
+
+const ReportForm = () => {
+    const form = useForm<z.infer<typeof ReportFormSchema>>({
+        resolver: zodResolver(ReportFormSchema),
+        defaultValues: {
+            subject: "",
+            description: "",
+        },
+    });
+    const handleSubmit = (val: z.infer<typeof ReportFormSchema>) => {
+        form.reset();
+    }
+    return (
+        <Card className="grid border-none ">
+            <CardHeader className="flex mb-4">
+                <CardTitle className="text-5xl font-['Inter']">Report</CardTitle>
+            </CardHeader>
+            <CardContent className="grid gap-4 ml-0 justify-self-start">
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-8">
+                        <FormField
+                            control={form.control}
+                            name="subject"
+                            render={({field}) => {
+                                return (
+                                    <FormItem className="flex flex-col ">
+                                        <FormLabel className="font-medium text-sm mr-[30px] inline-flex">
+                                            Subject
+                                        </FormLabel>
+                                        <FormControl>
+                                            <Input className="w-[350px]"
+                                                   type="text" {...field} placeholder="Subject"/>
+                                        </FormControl>
+                                        <FormMessage/>
+                                    </FormItem>
+                                );
+                            }}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="description"
+                            render={({field}) => (
+                                <FormItem className="flex flex-col">
+                                    <FormLabel className="text-sm font-medium">Description</FormLabel>
+                                    <FormControl>
+                                        <Textarea
+                                            className="w-[350px] h-[130px]"
+                                            placeholder="Enter a Description.."
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage/>
+                                </FormItem>
+                            )}
+                        />
+                        <Button
+                            type="submit"
+                            className="w-[100%]"
+                        >
+                            Submit Report
+                        </Button>
+                    </form>
+                </Form>
+            </CardContent>
+        </Card>
+
+    )
+}
+
+export default ReportForm;
---
+++ app/user/(routes)/photographer/[userId]/(routes)/profile/settings/page.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import Earnings from "./components/earnings";
+import AddCategory from "./components/add_category";
+import BankDetails from "./components/bank_details";
+import Report from "./components/report";
+import Faq from "./components/faq";
+import { Payment, columns } from "./components/paymentColumns";
+import { DataTable } from "./components/data-table";
+import { CalendarDemo } from "./components/calender";
+import { Button } from "@/components/ui/button";
+import { signOut } from "next-auth/react";
+
+const getData: Payment[] = [
+    {
+        clientName: "John Doe",
+        invoiceId: "INV-1234",
+        date: "Feb 2,2024",
+        paymentStatus: "Paid",
+        amount: 2500,
+        type: "Album Payment",
+    },
+    {
+        clientName: "Jane Doe",
+        invoiceId: "INV-1235",
+        date: "Jan 4,2024",
+        paymentStatus: "UnPaid",
+        amount: 1000,
+        type: "Booking",
+    },
+    {
+        clientName: "client name3",
+        invoiceId: "INV-1485",
+        date: "Jan 4,2024",
+        paymentStatus: "Pending",
+        amount: 1500,
+        type: "Booking",
+    },
+];
+
+export default function Settings() {
+    const [filteredUsers, setFilteredUsers] = useState<Payment[]>([]);
+    const [isMobile, setIsMobile] = useState<boolean>(window.innerWidth < 640);
+
+    useEffect(() => {
+        const fetchUsers = () => {
+            const data = getData;
+            setFilteredUsers(data);
+        };
+        fetchUsers();
+
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 640);
+        };
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
+
+    return (
+        <div className="flex flex-col items-center justify-between min-h-screen p-5">
+            <div className="relative flex flex-row items-end justify-end ml-[40%] lg:ml-[70%] gap-3">
+                <Button
+                    className="w-[120px] text-center bg-red-600"
+                    type="submit"
+                >
+                    Delete Account
+                </Button>
+                <Button
+                    onClick={() =>
+                        signOut({
+                            callbackUrl: `${window.location.origin}/sign-in`,
+                        })
+                    }
+                >
+                    Log Out
+                </Button>
+            </div>
+            <div className="grid mb-4 md:flex md:justify-center lg:gap-[40px]">
+                <div className="md:mb-0 md:mr-4">
+                    <Earnings />
+                </div>
+                <AddCategory />
+            </div>
+            <div className="lg:mx-[4%]">
+                <BankDetails />
+            </div>
+            {isMobile ? (
+                <CalendarDemo />
+            ) : (
+                <DataTable columns={columns} data={filteredUsers} />
+            )}
+            <div className="grid sm:grid-flow-col mb-4 sm:justify-stretch lg:w-[920px]">
+                <div className="md:mb-5">
+                    <Report />
+                </div>
+                <Faq />
+            </div>
+        </div>
+    );
+}
