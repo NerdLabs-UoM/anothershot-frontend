f4cbacffdd35a2d301328dc9f72342652bda993c
Kalhara JA <kalharajak.21@uom.lk>
2024-03-13 17:04:26 +0530

    feat(ANSH-113): chat system with attachment sending (#35)

--- components/chat/chatTopBar.tsx
+++ components/chat/chatTopBar.tsx
@@ -1,27 +1,51 @@
 import React, { useEffect } from 'react'
 import { useSession } from 'next-auth/react';
-import { Info } from 'lucide-react';
+import { Trash2, Info } from 'lucide-react';
 import Link from 'next/link';
 import { cn } from '@/app/lib/utils';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
-import { buttonVariants } from '@/components/ui/button';
+import { Button, buttonVariants } from '@/components/ui/button';
 import { Chat } from '@/app/lib/types';
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
 
 interface ChatTopbarProps {
     selectedChat: Chat;
 }
 
-export const TopbarIcons = [{ icon: Info }];
+export const TopbarIcons = [{ icon: Info }, { icon: Trash2 }];
 
 
 export default function ChatTopbar({ selectedChat }: ChatTopbarProps) {
 
     const { data: session } = useSession()
-    const [selectedUser, setSelectedUser] = React.useState(selectedChat.users.find(user => user.id !== session?.user.id))
+    const [selectedUser, setSelectedUser] = React.useState(selectedChat?.users.find(user => user.id !== session?.user.id))
 
     useEffect(() => {
-        setSelectedUser(selectedChat.users.find(user => user.id !== session?.user.id))
+        setSelectedUser(selectedChat?.users.find(user => user.id !== session?.user.id))
     }, [selectedChat, session?.user.id])
+
+    const handleDeleteChat = async () => {
+        try {
+            const userId = session?.user.id;
+            const chatId = selectedChat.id;
+            const res = await axios.delete(`${process.env.NEXT_PUBLIC_API_URL}/api/chat/delete/${userId}/${chatId}`);
+            toast.success('Chat deleted');
+        } catch (error) {
+            toast.error('Error deleting chat');
+        }
+    }
 
     return (
         <div className="w-full h-20 flex p-4 justify-between items-center border-b">
@@ -29,7 +53,7 @@ export default function ChatTopbar({ selectedChat }: ChatTopbarProps) {
                 <Avatar className="flex justify-center items-center">
                     <AvatarImage
                         src={selectedUser?.image}
-                        alt={selectedUser?.name}
+                        alt={selectedUser?.userName}
                         width={6}
                         height={6}
                         className="w-10 h-10 "
@@ -37,13 +61,13 @@ export default function ChatTopbar({ selectedChat }: ChatTopbarProps) {
                     <AvatarFallback>CN</AvatarFallback>
                 </Avatar>
                 <div className="flex flex-col">
-                    <span className="font-medium">{selectedUser?.name}</span>
+                    <span className="font-medium">{selectedUser?.userName}</span>
                     {/* <span className="text-xs">Active 2 mins ago</span> */}
                 </div>
             </div>
 
             <div>
-                {TopbarIcons.map((icon, index) => (
+                {/* {TopbarIcons.map((icon, index) => (
                     <Link
                         key={index}
                         href="#"
@@ -55,7 +79,31 @@ export default function ChatTopbar({ selectedChat }: ChatTopbarProps) {
                     >
                         <icon.icon size={20} className="text-muted-foreground" />
                     </Link>
-                ))}
+                ))} */}
+                <AlertDialog>
+                    <AlertDialogTrigger>
+                        <Button
+                            variant="destructive"
+                            size="icon"
+                            className="h-9 w-9"
+                        >
+                            <Trash2 size={20} className="text-white-foreground " />
+                        </Button>
+                    </AlertDialogTrigger>
+                    <AlertDialogContent>
+                        <AlertDialogHeader>
+                            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                            <AlertDialogDescription>
+                                This action cannot be undone. This will permanently delete your chat with {selectedUser?.userName}.
+                            </AlertDialogDescription>
+                        </AlertDialogHeader>
+                        <AlertDialogFooter>
+                            <AlertDialogCancel>Cancel</AlertDialogCancel>
+                            <AlertDialogAction onClick={handleDeleteChat}>Continue</AlertDialogAction>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialog>
+
             </div>
         </div>
     )
