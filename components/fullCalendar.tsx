1b73376a80c928ce4c0e792e672d8dbb5e743f27
Kalhara JA <kalharajak.21@uom.lk>
2024-03-16 19:29:16 +0530

    feat(ANSH-122): install Full Calnedar component (#38)

---
+++ components/fullCalendar.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import { Event } from '@/app/lib/types';
+
+interface FullCalendarProps {
+    events: Event[];
+}
+
+const FullCalendarComp: React.FC<FullCalendarProps> = (
+    events,
+) => {
+    return (
+        <FullCalendar
+            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+            initialView="dayGridMonth"
+            headerToolbar={{
+                left: 'prev,next today',
+                center: 'title',
+                right: 'dayGridMonth,timeGridWeek,timeGridDay'
+            }}
+            events={events}
+        />
+    )
+}
+
+export default FullCalendarComp;
