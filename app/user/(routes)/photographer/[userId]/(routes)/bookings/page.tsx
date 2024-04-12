ae44b0b300bf11fa813336bdf11f15e13fac0539
Amarasena B.H.L.M. <lahimalshi@gmail.com>
2024-04-12 16:09:39 +0530

    feat(ANSH-61): update bookings

--- app/user/(routes)/photographer/[userId]/(routes)/bookings/page.tsx
+++ app/user/(routes)/photographer/[userId]/(routes)/bookings/page.tsx
@@ -5,68 +5,75 @@ import React from "react";
 import { useSession } from "next-auth/react";
 import Offers from "@/components/offer/offers";
 
-function BookingPage() {
+// function BookingPage() {
+//   const { data: session } = useSession();
+//   const userId  = session?.user.id
+//   return (
+//     <div className = "flex flex-col justify-center">
+//       <Offers/>
+//     </div>
+//   );
+// }
+
+// export default BookingPage;
+
+import { Event } from "@/app/lib/types";
+import FullCalendarComp from "@/components/fullCalendar";
+import { useState } from "react";
+import { useParams } from "next/navigation";
+import Form from '../bookings/components/forms';
+
+const sampleEvents: Event[] = [
+  {
+    id: '1',
+    title: 'event 1',
+    start: new Date(2024, 2, 1, 19, 30,),
+    end: new Date(2024, 2, 1, 21, 30),
+    allDay: false,
+  },
+  {
+    id: '2',
+    title: 'event 2',
+    start: new Date(2024, 2, 2, 17, 30,),
+    end: new Date(2024, 2, 2, 19, 30),
+    allDay: false,
+  },
+  {
+    id: '3',
+    title: 'event 3',
+    start: new Date(2024, 2, 2, 19, 30,),
+    end: new Date(2024, 2, 2, 21, 30),
+    allDay: false,
+  },
+  {
+    id: '4',
+    title: 'event 4',
+    start: new Date(),
+    end: new Date(),
+    allDay: true,
+  },
+]
+
+const PhotographerBookingsPage = () => {
+
+  const { userId } = useParams();
   const { data: session } = useSession();
-  const userId  = session?.user.id
+  const [events, setEvents] = useState<Event[]>(sampleEvents)
+
   return (
-    <div className = "flex flex-col justify-center">
-      <Offers/>
-    </div>
+    <div className="w-full flex justify-center py-5" >
+      <div className="lg:w-3/5 ">
+        <FullCalendarComp
+          events={events}
+        />
+      </div>
+      <div className="flex pr-20">
+        {session && session.user && session.user.id === userId &&
+          <Form />}
+      </div>
+    </div >
   );
 }
 
-export default BookingPage;
-
-
-// import { Event } from "@/app/lib/types";
-// import FullCalendarComp from "@/components/fullCalendar";
-// import { useState } from "react";
-
-// const sampleEvents: Event[] = [
-//     {
-//         id: '1',
-//         title: 'event 1',
-//         start: new Date(2024, 2, 1, 19, 30,),
-//         end: new Date(2024, 2, 1, 21, 30),
-//         allDay: false,
-//     },
-//     {
-//         id: '2',
-//         title: 'event 2',
-//         start: new Date(2024, 2, 2, 17, 30,),
-//         end: new Date(2024, 2, 2, 19, 30),
-//         allDay: false,
-//     },
-//     {
-//         id: '3',
-//         title: 'event 3',
-//         start: new Date(2024, 2, 2, 19, 30,),
-//         end: new Date(2024, 2, 2, 21, 30),
-//         allDay: false,
-//     },
-//     {
-//         id: '4',
-//         title: 'event 4',
-//         start: new Date(),
-//         end: new Date(),
-//         allDay: true,
-//     },
-// ]
-
-
-// const PhotographerBookingsPage = () => {
-//     const [events, setEvents] = useState<Event[]>(sampleEvents)
-
-//     return (
-//         <div className="w-full flex justify-center py-5" >
-//             <div className="lg:w-3/5 ">
-//                 <FullCalendarComp
-//                     events={events}
-//                 />
-//             </div>
-//         </div >
-//     );
-// }
-
-// export default PhotographerBookingsPage;
+export default PhotographerBookingsPage;
 
