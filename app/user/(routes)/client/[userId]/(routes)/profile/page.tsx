df3b3b694cbe46589d796778aa100b92da835a1b
PawaniGimhani <125270084+PawaniGimhani@users.noreply.github.com>
2024-04-15 14:29:18 +0530

    style(ANSH-135): update styles in client profile

--- app/user/(routes)/client/[userId]/(routes)/profile/page.tsx
+++ app/user/(routes)/client/[userId]/(routes)/profile/page.tsx
@@ -12,7 +12,7 @@ const ClientProfile = () => {
     }, []);
   
     return (
-      <div className="flex flex-col lg:flex-row items-center mt-4 sm:mt-12 lg:mt-5 lg:mx-16">
+      <div className="flex flex-col min-h-screen lg:flex-row items-center mt-4 sm:mt-12 lg:mt-5 lg:mx-16">
         {isClient && (
           <>
             <ProfileBio />
