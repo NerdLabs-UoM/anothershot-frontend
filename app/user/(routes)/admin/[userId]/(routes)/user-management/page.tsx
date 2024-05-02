8f299e64a58ab6b1ec0097a2d75cc1587c010db3
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2024-05-02 14:40:13 +0530

    feat(ANSH-172): add user filter option in user management table of users

--- app/user/(routes)/admin/[userId]/(routes)/user-management/page.tsx
+++ app/user/(routes)/admin/[userId]/(routes)/user-management/page.tsx
@@ -6,6 +6,27 @@ import { DataTable } from "./components/data-table";
 import { fetchData, fetchLastPage } from "./serviceData";
 import PaginationSection from "./components/pagination";
 import { User } from "@/app/lib/types";
+import { set } from "lodash";
+
+interface UserRole {
+  role: string;
+  isChecked: boolean;
+}
+
+const userRoles: UserRole[] = [
+  {
+    role: "Admin",
+    isChecked: false,
+  },
+  {
+    role: "Photographer",
+    isChecked: false,
+  },
+  {
+    role: "Client",
+    isChecked: false,
+  },
+];
 
 const AdminPage = () => {
   const [page, setPage] = useState(1);
@@ -15,24 +36,36 @@ const AdminPage = () => {
   const [name, setName] = useState<string>("");
   const [fetch, setFetch] = useState<boolean>(false);
 
+  const [userRole, setUserRole] = useState<UserRole[]>(userRoles);
+const [rolesString,setRolesString] = useState<string>("ADMIN,PHOTOGRAPHER,CLIENT");
+
+  const makeStringRoles = (roles: UserRole[]) => {
+    const newUserRole = roles.filter((role) => role.isChecked);
+
+    return newUserRole.map((user) => user.role).join(",").toUpperCase();
+  }
   useEffect(() => {
     setPage(1);
   }, [fetch]);
 
   useEffect(() => {
+    console.log("user roles tika", makeStringRoles(userRole))
     const fetchUsers = async () => {
-      const data:User[] = await fetchData(page,name);
+      const data: User[] = await fetchData(page, name,rolesString);
       setFilteredUsers(data);
     };
     fetchUsers();
-  }, [page,fetch]);
+  }, [page, fetch]);
 
   useEffect(() => {
+    console.log("user roles tika", makeStringRoles(userRole))
+
     const fetchLast = async () => {
-      const data = await fetchLastPage(name);
+      const data = await fetchLastPage(name, rolesString);
       setLast(data);
     };
     fetchLast();
+    // console.log("user roles tika", newUserRole)
   }, [fetch]);
 
   const handlePrev = () => {
@@ -48,36 +81,43 @@ const AdminPage = () => {
   };
 
   const handleSearchValueChange = (value: string) => {
-    if (!value) {
+    if (value) {
       setFetch(!fetch);
     }
     setName(value);
   };
 
-  const handleSearch = () => {
-    setFetch(!fetch);
-
-  }
-
   const handleClick = (currentPage: number) => {
     setPage(currentPage);
   };
 
+
+  const onCheckInputChange = (newUserRole: UserRole[]) => {
+    if (newUserRole) {
+      setFetch(!fetch);
+    }
+    setUserRole(newUserRole);
+    setRolesString(makeStringRoles(newUserRole));
+    // console.log("newuserRole", newUserRole);
+  };
+
+
   return (
     <div className="w-full">
-        <DataTable 
-        columns={columns} 
-        onSearchClick={handleSearch}
+      <DataTable
+        columns={columns}
         onSearchValueChange={handleSearchValueChange}
+        onCheckInputChange={onCheckInputChange}
+        userRole={userRole}
         data={filteredUsers} />'
-        
-        <PaginationSection
-          lastPage={last}
-          currentPage={page}
-          handlePrev={handlePrev}
-          handleNext={handleNext}
-          handleClick={handleClick}
-        />
+
+      <PaginationSection
+        lastPage={last}
+        currentPage={page}
+        handlePrev={handlePrev}
+        handleNext={handleNext}
+        handleClick={handleClick}
+      />
     </div>
   );
 };
