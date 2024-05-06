b779fe451998d4e9b3177b6b123eef75f5b5c24e
Kalhara JA <kalhara.ja@gmail.com>
2024-05-06 22:23:17 +0530

    fix(ANSH-61): fix conflict issues

--- app/user/(routes)/photographer/[userId]/(routes)/bookings/page.tsx
+++ app/user/(routes)/photographer/[userId]/(routes)/bookings/page.tsx
@@ -4,7 +4,7 @@
 import React, { useEffect } from "react";
 import { useSession } from "next-auth/react";
 import AddBooking from "./components/AddBooking";
-import { useParams } from "next/navigation";
+// import { useParams } from "next/navigation";
 import FetchBookings from "./components/fetchBookings";
 
 // function BookingPage() {
@@ -132,18 +132,18 @@ const PhotographerBookingsPage = () => {
 
 export default PhotographerBookingsPage;
 
-<!-- function BookingPage() {
-  const { data: session } = useSession();
-  const  ParamuserId  = useParams();
-  const userId  = session?.user.id
-  console.log(ParamuserId);
-  console.log(userId);
-  return (
-    <div className = "flex flex-col justify-center items-center">
-      {ParamuserId.userId ==userId?<FetchBookings/>:<AddBooking/>}
-    </div>
-  );
-}
+// function BookingPage() {
+//   const { data: session } = useSession();
+//   const  ParamuserId  = useParams();
+//   const userId  = session?.user.id
+//   console.log(ParamuserId);
+//   console.log(userId);
+//   return (
+//     <div className = "flex flex-col justify-center items-center">
+//       {ParamuserId.userId ==userId?<FetchBookings/>:<AddBooking/>}
+//     </div>
+//   );
+// }
 
-export default BookingPage; -->
+// export default BookingPage;
 
