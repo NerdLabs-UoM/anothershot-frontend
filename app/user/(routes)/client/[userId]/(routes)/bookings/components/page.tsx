bffb30f17fe426eb6a831fa09f84bc130f73dbe7
PawaniGimhani <125270084+PawaniGimhani@users.noreply.github.com>
2024-05-10 02:52:39 +0530

    feat(ANSH-182): make client prof view only client

--- app/user/(routes)/client/[userId]/(routes)/bookings/components/page.tsx
+++ app/user/(routes)/client/[userId]/(routes)/bookings/components/page.tsx
@@ -3,25 +3,42 @@
 import { useEffect, useState } from "react";
 import ProfileBio from "./ProfileBio";
 import ClientBookings from "./ClientBookings";
+import { useParams, useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
 
 const Bookings = () => {
-    const [isClient, setIsClient] = useState(false);
-  
-    useEffect(() => {
-      setIsClient(true);
-    }, []);
-  
-    return (
+  const [isClient, setIsClient] = useState(false);
+  const { userId } = useParams();
+  const { data: Session } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  const ClientProfile = () => {
+    if (Session && Session?.user?.id === userId) {
+      return (
       <div className="flex flex-col min-h-screen items-center lg:flex-row lg:items-start mt-4 sm:mt-12 lg:pt-20 lg:mx-16">
         {isClient && (
           <>
             <ProfileBio />
-            <ClientBookings/>
+            <ClientBookings />
           </>
         )}
       </div>
-    );
+      );
+    }else{
+      router.push('/page-not-found');
+    } 
+    ;
   };
-  
-  export default Bookings;
-  
+
+  return (
+    <div>
+        { ClientProfile() }
+    </div>
+  );
+};
+
+export default Bookings;
