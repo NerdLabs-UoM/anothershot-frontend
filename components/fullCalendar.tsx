4c44c83026b124f984e581c7a2ef72a2e0608cda
Kalhara JA <kalhara.ja@gmail.com>
2024-06-05 00:46:18 +0530

    fix: resolve build issues

--- components/fullCalendar.tsx
+++ components/fullCalendar.tsx
@@ -4,14 +4,24 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import { Event } from '@/app/lib/types';
+import { EventInput } from '@fullcalendar/core/index.js';
 
 interface FullCalendarProps {
     events: Event[];
 }
 
-const FullCalendarComp: React.FC<FullCalendarProps> = (
-    events,
-) => {
+const mapEventToEventInput = (event: Event): EventInput => ({
+    id: event.id,
+    title: event.title,
+    start: event.start ? event.start.toISOString() : undefined,
+    end: event.end ? event.end.toISOString() : undefined,
+    allDay: event.allDay,
+    description: event.description,
+});
+
+const FullCalendarComp: React.FC<FullCalendarProps> = ({ events }) => {
+    const eventInputs: EventInput[] = events.map(mapEventToEventInput);
+
     return (
         <FullCalendar
             plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
@@ -21,9 +31,9 @@ const FullCalendarComp: React.FC<FullCalendarProps> = (
                 center: 'title',
                 right: 'dayGridMonth,timeGridWeek,timeGridDay'
             }}
-            events={events}
+            events={eventInputs}
         />
-    )
-}
+    );
+};
 
 export default FullCalendarComp;
