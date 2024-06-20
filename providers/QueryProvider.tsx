dcf0e17d381147a73ff7470a6755d196058fdccb
Kavindu Wijegunasekara <110486479+Kavinduprabasara@users.noreply.github.com>
2024-06-20 23:19:01 +0530

    Bugfix/ANSH-242/session-unmount-bug (#105)
    * fix: change auth provider
    
    * fix: update session provider
    
    ---------
    
    Co-authored-by: Kalhara JA <kalhara.ja@gmail.com>

---
+++ providers/QueryProvider.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import React from "react";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
+export function QueryProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      {children}
+    </QueryClientProvider>
+  );
+}
+
+export default QueryProvider;
