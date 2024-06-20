0662996378a18bd50988f6c40d3500134386100d
Kalhara JA <kalhara.ja@gmail.com>
2024-06-20 23:08:05 +0530

    fix: change auth provider

--- providers/AuthProvider.tsx
+++ providers/AuthProvider.tsx
@@ -1,4 +1,38 @@
 "use client";
 
-import { SessionProvider } from "next-auth/react";
-export default SessionProvider;
+import React, { useEffect, useState, ReactNode } from "react";
+import { SessionProvider, useSession } from "next-auth/react";
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null;
+  }
+
+  return (
+    <SessionProvider>
+      <AuthWrapper>{children}</AuthWrapper>
+    </SessionProvider>
+  );
+};
+
+const AuthWrapper: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const { status } = useSession();
+
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+
+  return <>{children}</>;
+};
+
+export default AuthProvider;
